cff2f646d69ec5d39dfd522fb4b7ed8c0dc2312c
Gary Maditz <gmaditz@comcast.net>
2012-04-22 22:21:43 -0400

    Round two of Chapter 10 Exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,8 +48,13 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
+    user = User.find(params[:id])
+    if (user == current_user) && (current_user.admin?)
+      flash[:error] = "Admin user cannout destroy self"
+    else
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+    end  
     redirect_to users_path
   end
   
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -314,8 +314,14 @@ describe UsersController do
     describe "as an admin user" do
     
       before(:each) do
-        admin = Factory(:user, :email => "admin@example.com", :admin => true)
-        test_sign_in(admin)
+        @admin = Factory(:user, :email => "admin@example.com", :admin => true)
+        test_sign_in(@admin)
+      end
+      
+      it "should not destroy the admin user" do
+        lambda do
+          delete :destroy, :id => @admin
+        end.should_not change(User, :count)
       end
       
       it "should destroy the user" do
